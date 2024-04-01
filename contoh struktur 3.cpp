2fae95938e1eaebaa56147874c3fa8d3e89a8ec2
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-04-01 15:33:20 +0700

    cout alamat mahasiswa

--- contoh struktur 3.cpp
+++ contoh struktur 3.cpp
@@ -21,6 +21,24 @@ int main() {
     cout << "Nama Mahasiswa : ";
     getline(cin , mhs.nama);
 
+    cout << "Alamat Mahasiswa : ";
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+
+    cout << "Umur  Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : ";
+    cout << "\n \t Desa : " << mhs.alamat.desa;
+    cout << "\n \t Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
+
 }    
 
     
