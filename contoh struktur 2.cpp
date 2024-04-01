ce4e59b44b0e91a3b01480e6118d0e2eccb83e95
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-04-01 15:00:58 +0700

    Menuliskan objek Mahasiswa

--- contoh struktur 2.cpp
+++ contoh struktur 2.cpp
@@ -14,4 +14,20 @@ struct Mahasiswa {
 };
 
 
-int main() 
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur  Mahasiswa : ";
+    cin >> mhs.umur;
+}
+   
+
+
+    
