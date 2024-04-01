172ecdc081dbd0122995641681f19075a251670f
alifinrayhan <160594805+alifinrayhan@users.noreply.github.com>
2024-04-01 16:24:31 +0700

    Menambah \n di Alamat

--- contoh struktur 3.cpp
+++ contoh struktur 3.cpp
@@ -22,9 +22,9 @@ int main() {
     getline(cin , mhs.nama);
 
     cout << "Alamat Mahasiswa : ";
-    cout << "\t Nama Desa : ";
+    cout << "\n\t Nama Desa : ";
     cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
+    cout << "\n\t Nama Kota : ";
     cin >> mhs.alamat.kota;
 
 
